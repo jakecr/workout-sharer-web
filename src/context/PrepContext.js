af3358ccf236e23a99e78dc4d85e19972cf46c1f
Jacob <jake6112002@gmail.com>
2020-03-03 18:33:07 -0500

    Patch 2.1

--- src/context/PrepContext.js
+++ src/context/PrepContext.js
@@ -27,6 +27,10 @@ const planReducer = (state, action) => {
 }
 
 const checkIfLoggedIn = dispatch => async () => {
+    if(window.location.hostname == 'workout-sharer-web.herokuapp.com' || window.location.protocol !== 'https:') {
+        window.location.href = 'https://www.workoutsharer.com' + window.location.pathname
+    }
+
     const token = Cookies.get('token')
 
     try {
@@ -68,6 +72,10 @@ const checkIfLoggedIn = dispatch => async () => {
 }
 
 const checkIfNotLoggedIn = dispatch => async () => {
+    if(window.location.hostname == 'workout-sharer-web.herokuapp.com' || window.location.protocol !== 'https:') {
+        window.location.href = 'https://www.workoutsharer.com' + window.location.pathname
+    }
+
     const token = Cookies.get('token')
     
     try {
