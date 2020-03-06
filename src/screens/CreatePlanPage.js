ce4bb131d75e5815fa038067e50849dd493bbb55
Jacob <jake6112002@gmail.com>
2020-03-06 18:40:45 -0500

    Patch 3.5

--- src/screens/CreatePlanPage.js
+++ src/screens/CreatePlanPage.js
@@ -472,7 +472,7 @@ const CreatePlanPage = () => {
                                                                     name='Rate of Perceived Exertion' 
                                                                     type='text' 
                                                                     value={workoutItem.repsForAssessment} 
-                                                                    onChange={(e) => e.target.value.match(/^10|[4-9]$/) && onChangeBasicWorkout({ day: dayIndex, workout: workoutIndex, key: 'repsForAssessment', value: e.target.value })} 
+                                                                    onChange={(e) => !e.target.value.match(/^10|[4-9]$/) && onChangeBasicWorkout({ day: dayIndex, workout: workoutIndex, key: 'repsForAssessment', value: e.target.value })} 
                                                                 />
                                                                 , <Input 
                                                                     key={(dayIndex * workoutIndex + .1) * .11111}
@@ -781,7 +781,7 @@ const CreatePlanPage = () => {
                                                                                 name='Rate of Perceived Exertion' 
                                                                                 type='text' 
                                                                                 value={setItem.repsForAssessment} 
-                                                                                onChange={(e) => e.target.value.match(/^10|[4-9]$/) && onChangeSet({ day: dayIndex, workout: workoutIndex, set: setIndex, key: 'repsForAssessment', value: e.target.value })} 
+                                                                                onChange={(e) => !e.target.value.match(/^10|[4-9]$/) && onChangeSet({ day: dayIndex, workout: workoutIndex, set: setIndex, key: 'repsForAssessment', value: e.target.value })} 
                                                                             />
                                                                             , <Input 
                                                                                 key={(dayIndex * workoutIndex + .1) * .11111}
